0a1f0dc003a7eed1061dc53e58bf85a3c0b94ece
Allegra <aukseallegra@gmail.com>
2018-10-21 21:43:41 +0100

    cookies setup

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,17 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+  .then(user => {
+    done(null, user)
+  })
+})
+
 passport.use(
   new GoogleStrategy(
     {
@@ -13,9 +24,17 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleId: profile.id }).save();
-      // console.log("accessToken: ", accessToken);
-      // console.log("profile: ", profile);
+      //console.log('Profile: ', profile)
+      User.findOne({ googleId: profile.id })
+      .then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);  //no err, we have a record
+        }else {
+          new User({ googleId: profile.id })
+          .save()
+          .then(user => done(null, user))  //record we just created
+        }
+      })
     }
   )
 );
