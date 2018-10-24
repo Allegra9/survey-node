ffcf4783355526b1c7f7aa9597026d297b98b75d
Allegra <aukseallegra@gmail.com>
2018-10-24 21:31:05 +0100

    added another proxy route, axios and redux thunk

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
+import reduxThunk from 'redux-thunk'
 
 import App from './components/App'
 import reducers from './reducers'
 
-const store = createStore(reducers, {}, applyMiddleware())
-//reducer, initial state
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk))
+//reducer, initial state, middleware 
 
 ReactDOM.render(
   <Provider store={store}>
