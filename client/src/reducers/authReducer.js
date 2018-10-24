0656b2446061e626387f8409858633bbf035f5d1
Allegra <aukseallegra@gmail.com>
2018-10-24 22:46:21 +0100

    authReducer checks if User logged in

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,7 +1,14 @@
-export default function(state = {}, action) {
-  console.log("action in authReducer: ",action)
+import { FETCH_USER } from '../actions/types'
+
+export default function(state = null, action) {
   switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false
     default:
       return state
   }
 }
+
+//null - awaiting promise to resolve
+//action.payload - user model 
+//false - user is not logged in
